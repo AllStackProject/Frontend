7d6f5dbccf49a43378ee3d3e08bf4939520e2934
ddddabi <ii887742@gmail.com>
2025-10-23 11:26:53 +0900

    feat: admin ui - 동영상별 시청 관리 탭 구현

---
+++ src/pages/Admin/WatchHistoryPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VideoWatchSection from "@/components/Admin/History/VideoWatchSection";
+
+const WatchHistoryPage: React.FC = () => {
+  return (
+    <div className="p-6">
+      <VideoWatchSection />
+    </div>
+  );
+};
+
+export default WatchHistoryPage;

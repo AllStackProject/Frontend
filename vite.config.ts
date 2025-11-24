010b7b2b1cc6fcedc7b0d5e77bb58fc94022d314
ddddabi <ii887742@gmail.com>
2025-11-24 16:06:40 +0900

    fix: 도메인 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',          // 127.0.0.1 뿐 아니라 privideo.cloud 도 수용
+    port: 5173,
+    hmr: {
+      host: 'privideo.cloud', // 브라우저에서 접근하는 도메인
+      protocol: 'ws',         // dev니까 ws
+      port: 5173,
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
